afceb4f0ae7bb7fa7c42ff1d737c558cb0b1368c
Ansel Bridgewater <iamansel@gmail.com>
2019-08-15 20:31:09 -0700

    Added other methods for API wrapper

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 const got = require('got');
 
-async function randomFact() {
+export async function randomFact() {
   try {
     const response = await got('http://numbersapi.com/random/');
-    console.log(response.body);
+    return response.body;
   } catch (error) {
-    console.log(error.response.body);
+    return error.response.body;
   }
 }
 
-randomFact()
+export async function triviaFact(number = 'random') {
+  try {
+    const response = await got(`http://numbersapi.com/${number}/trivia`);
+    return response.body;
+  } catch (error) {
+    return error.response.body;
+  }
+}
+
+export async function mathFact(number = 'random') {
+  try {
+    const response = await got(`http://numbersapi.com/${number}/math`);
+    return response.body;
+  } catch (error) {
+    return error.response.body;
+  }
+}
+
+export async function yearFact(number = 'random') {
+  try {
+    const response = await got(`http://numbersapi.com/${number}/year`);
+    return response.body;
+  } catch (error) {
+    return error.response.body;
+  }
+}
+
+export async function dateFact(month = 'random', day = '') {
+  try {
+    const response = await got(`http://numbersapi.com/${month}/${day}/date`);
+    return response.body;
+  } catch (error) {
+    return error.response.body;
+  }
+}
